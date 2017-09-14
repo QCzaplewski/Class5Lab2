cffde9ab24907073c0095ebb1f075573cc09481a
Quinten <Quinten@192.168.50.1>
2017-09-14 12:28:39 -0500

    Task 2: Added to the main class to test the methods of the Car class.

--- src/task2/class5lab2.java
+++ src/task2/class5lab2.java
@@ -11,6 +11,17 @@ package task2;
  */
 public class class5lab2 {
     public static void main(String[] args) {
+        Chevy car1 = new ChevyMalibu(2004, "Blue");
         
+        Chevy car2 = new Chevy("Cruze", 2003, "Purple");
+        
+        System.out.println(car1.getMake() + " " + car1.getModel());
+        System.out.println(car2.getMake() + " " + car2.getModel());
+        
+        car1.turnOnEngine();
+        car2.turnOnEngine();
+        
+        car1.turnOffEngine();
+        car2.turnOffEngine();
     }
 }
